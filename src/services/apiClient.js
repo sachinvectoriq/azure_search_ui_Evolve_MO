43dd3f92e14850f8b1530e831f5c3dda9ff24d04
Solomon Bhaskar <131343533+solomon712@users.noreply.github.com>
2025-06-05 11:08:04 +0530

    Update apiClient.js

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apiClient = axios.create({
   baseURL:
-    import.meta.env.VITE_API || 'https://qa-azure-search.azurewebsites.net',
+    import.meta.env.VITE_API || 'app-azuresearch-qa-emo.azurewebsites.net', //API HAS BEEN CHANGED
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
